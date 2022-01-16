9b5ee7f7b2383064e7176d441f53f5199c7b24ac
leriche213 <leriche213@091225b4-4d92-4632-96f6-57cb08195bd5>
2022-01-16 17:51:05 +0000

    Ajout de la souris et d'un fond d'écran

--- interface.py
+++ interface.py
@@ -1,14 +1,31 @@
-from pygame import *
+import pygame
+pygame.init()
 
 class Interface:            # À compléter plus tard
     def __init__(self, largeur = 800, hauteur = 450):
-        self.frame = display.set_mode((largeur, hauteur), vsync=0)
+        pygame.display.set_caption("Card Game")
+        self.frame = pygame.display.set_mode((largeur, hauteur), vsync=0)
         self.loop()
+
+
     
     def loop(self):
         self.alive = True
         while self.alive:
-            for e in event.get() :
-                if e.type == QUIT:
+            pygame.mouse.set_visible(False)
+            fecran = pygame.image.load('images/table.jpg')                # Fond d'ecran à modifier
+            curseur = pygame.image.load('images/Cotton_gloves.png')
+            curseur = pygame.transform.scale(curseur, (50, 50))           # Permet de changer la taille du curseur
+            curseur = pygame.transform.rotate(curseur, -90)              # Change la rotation
+            self.frame.blit(fecran, (0, 0))                               # Commande qui permet de mettre le fond d'ecran sur l'image
+            x, y = pygame.mouse.get_pos()                                 # J'obtient les positions x et y de ma souris
+            self.frame.blit(curseur, (x -20, y -20))                       #Permet de cadrer l'image
+
+
+            for e in pygame.event.get() :
+                if e.type == pygame.QUIT:
                     self.alive = False
-            display.flip()
+            pygame.display.flip()
+
+
+test = Interface()
