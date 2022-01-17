752671aceff25ad2b14771ccf992fea803765915
leriche213 <leriche213@091225b4-4d92-4632-96f6-57cb08195bd5>
2022-01-17 21:40:41 +0000

    création d'une nouvelle image pour le click droit + ajout à l'interface ( & suppression de l'ancienne image du click droit)

--- interface.py
+++ interface.py
@@ -15,17 +15,21 @@ class Interface:            # À compléter plus tard
             pygame.mouse.set_visible(False)
             fecran = pygame.image.load('images/table.jpg')                  # Fond d'ecran à modifier
             curseur = pygame.image.load('images/Cotton_gloves.png')
-            self.frame.blit(fecran, (0, 0))                                 # Commande qui permet de mettre le fond d'ecran sur l'image
-
-
-            pression = pygame.mouse.get_pressed(3)                          
-            if pression[0] == True :
-                curseur = pygame.image.load('images/Cotton_gloves2.png')
-
-
+            self.frame.blit(fecran, (0, 0))                                 # Commande qui permet de mettre le fond d'ecran sur l'Interface
             curseur = pygame.transform.scale(curseur, (50, 50))             # Permet de changer la taille du curseur
-            curseur = pygame.transform.rotate(curseur, -90)                 # Change la rotation
             x, y = pygame.mouse.get_pos()                                   # J'obtient les positions x et y de ma souris
+
+
+            pression = pygame.mouse.get_pressed(3)
+            if pression[0] == True :
+                curseur = pygame.image.load('images/Cotton_gloves_update.png')
+                curseur = pygame.transform.scale(curseur, (27, 27))
+                x = x + 12
+                y = y + 12                                                  #Permet d'ajuster la position du nouveau gantelet
+            elif pression[1] == True :                                      #A faire : Gerer le click droit
+                pass
+
+            curseur = pygame.transform.rotate(curseur, -90)                 # Change la rotation
             self.frame.blit(curseur, (x - 20, y - 20))                      # Permet de cadrer l'image
 
 
