6e270deaaa310f633e17a5a4719cb613b628706b
leriche213 <leriche213@091225b4-4d92-4632-96f6-57cb08195bd5>
2022-01-17 09:40:40 +0000

    Ajout de la detection du click droit + ajout d'une variante des gants

--- interface.py
+++ interface.py
@@ -15,11 +15,19 @@ class Interface:            # À compléter plus tard
             pygame.mouse.set_visible(False)
             fecran = pygame.image.load('images/table.jpg')                # Fond d'ecran à modifier
             curseur = pygame.image.load('images/Cotton_gloves.png')
-            curseur = pygame.transform.scale(curseur, (50, 50))           # Permet de changer la taille du curseur
-            curseur = pygame.transform.rotate(curseur, -90)              # Change la rotation
             self.frame.blit(fecran, (0, 0))                               # Commande qui permet de mettre le fond d'ecran sur l'image
-            x, y = pygame.mouse.get_pos()                                 # J'obtient les positions x et y de ma souris
-            self.frame.blit(curseur, (x -20, y -20))                       #Permet de cadrer l'image
+
+
+            pression = pygame.mouse.get_pressed(num_buttons = 3)
+            if pression[0] == True :
+                curseur = pygame.image.load('images/Cotton_gloves2.png')
+
+
+            curseur = pygame.transform.scale(curseur, (50, 50))  # Permet de changer la taille du curseur
+            curseur = pygame.transform.rotate(curseur, -90)  # Change la rotation
+            x, y = pygame.mouse.get_pos()                               # J'obtient les positions x et y de ma souris
+            self.frame.blit(curseur, (x - 20, y - 20))  # Permet de cadrer l'image
+
 
 
             for e in pygame.event.get() :
