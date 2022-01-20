68730e88727e32ea5fc086b2541e34d6cdbea1c9
bouroui211 <bouroui211@091225b4-4d92-4632-96f6-57cb08195bd5>
2022-01-20 13:00:38 +0000

    changements sur la classe carte

--- carte.py
+++ carte.py
@@ -8,74 +8,66 @@ class Carte(pygame.sprite.Sprite):
         self.enseigne = enseigne        # la couleur de la carte
         self.valeur = valeur            # la valeur de la carte (as = 1, valet = 11, dame=  12, roi = 13, et joker = 0)
         self.retourné = False           # Vrai si la carte est face caché
+
+        self.updateSprite()         
+
+        self.rect = self.image.get_rect()       #self.image est crée dans updateSprite()
+
+
+    def updateSprite(self):                     # permet d'update le sprite
+        if self.retourné:
+            self.image = pygame.image.load("images/cartes 2 (avec Jokers)/dos-bleu.png")
+        else :
+            self.image = pygame.image.load(self.calcAddrFace())
     
-        
-
-        ens = ""
-        val = ""
-
+    def calcAddrFace(self):             # permet de calculer le nom de l'image a utiliser dans le fichier à l'aide de l'enseigne et de la valeur
+        addrFace = "images/cartes 2 (avec Jokers)/"
         if self.valeur == 0:
-            self.image = pygame.image.load("images/cartes 2 (avec Jokers)/ace_of_hearts.png")
+            addrFace += "black_joker.png"
         else:
-            if self.enseigne == "pique":
-                ens = "spades"
-            elif self.enseigne == "coeur":
-                ens = "hearts"
-            elif self.enseigne == "treifle":
-                ens = "clubs"
-            elif self.enseigne == "carreau":
-                ens = "diamonds"
-
             if self.valeur == 1:
-                val = "ace"
+                addrFace += "ace"
+            
             elif self.valeur <= 10:
-                val = str(self.valeur)
+                addrFace += str(self.valeur)
+            
             elif self.valeur == 11:
-                val = "jack"
+                addrFace += "jack"
+            
             elif self.valeur == 12:
-                val = "queen"
+                addrFace += "queen"
+            
             elif self.valeur == 13:
-                val = "king"
-            #print("images/cartes 2 (avec Jokers)/"+val+'_of_'+ens+".png")
-            self.image = pygame.image.load("images/cartes 2 (avec Jokers)/"+val+'_of_'+ens+".png")
+                addrFace += "king"
+            
+            else:
+                addrFace += str(val)
 
-        self.rect = self.image.get_rect()
+            addrFace += "_of_"
+            if self.enseigne == "pique":
+                addrFace += "spades"
+            elif self.enseigne == "coeur":
+                addrFace += "hearts"
+            
+            elif self.enseigne == "treifle":
+                addrFace += "clubs"
+            
+            elif self.enseigne == "carreau":
+                addrFace += "diamonds"
+            
+        return addrFace
 
-        #pygame.draw.rect(self.image, pygame.Rect(0,0, 20, 10))
+
 
     def estFigure(self):                # retourne vrai si la carte est un valet, une dame, ou un roi
         return self.valeur > 10
     
     def retourner(self):                # permet de retourner la carte (swap de True à False ou de False à True)
-        if self.retourné:
-            if self.valeur == 0:
-                self.image = pygame.image.load("images/cartes 2 (avec Jokers)/black_joker.png")
-            else:
-                if self.enseigne == "pique":
-                    ens = "spades"
-                elif self.enseigne == "coeur":
-                    ens = "hearts"
-                elif self.enseigne == "treifle":
-                    ens = "clubs"
-                elif self.enseigne == "carreau":
-                    ens = "diamonds"
-
-                if self.valeur == 1:
-                    val = "ace"
-                elif self.valeur <= 10:
-                    val = str(self.valeur)
-                elif self.valeur == 11:
-                    val = "jack"
-                elif self.valeur == 12:
-                    val = "queen"
-                elif self.valeur == 13:
-                    val = "king"
-                #print("images/cartes 2 (avec Jokers)/"+val+'_of_'+ens+".png")
-                self.image = pygame.image.load("images/cartes 2 (avec Jokers)/"+val+'_of_'+ens+".png")
-        else:
-            self.image = pygame.image.load("images/cartes 2 (avec Jokers)/dos-bleu.png")
-
         self.retourné = not self.retourné
+        self.updateSprite()
+        
+    
+        
 
 
     
