aea16b7394cffb9db89b0c45b34eb4796708a17a
bouroui211 <bouroui211@091225b4-4d92-4632-96f6-57cb08195bd5>
2022-01-27 13:59:58 +0000

    vesion bouton de son

--- interface.py
+++ interface.py
@@ -12,14 +12,31 @@ class Interface:            # À compléter plus tard
         self.fecran = pygame.transform.scale(self.fecran, (largeur, hauteur))
         self.frame = pygame.display.set_mode((largeur, hauteur))        # Rajouter pygame.FULLSCREEN pour mettre en plein ecran
         self.souris = Souris()
+
         self.bouton_quitter = pygame.image.load("images/Boutton_quitter.png")
         self.bouton_quitter = pygame.transform.scale(self.bouton_quitter, (60, 60))
         self.bouton_quitter = Bouton(largeur - 60,0,self.bouton_quitter)
+
+        self.bouton_stopmusique = pygame.image.load("images/stopMusique.png")
+        self.bouton_stopmusique = pygame.transform.scale(self.bouton_stopmusique, (60, 60))
+        self.bouton_stopmusique = Bouton(largeur - 120,0,self.bouton_stopmusique)
+
+        self.bouton_musiqueOn = pygame.image.load("images/musiqueOn.png")
+        self.bouton_musiqueOn = pygame.transform.scale(self.bouton_musiqueOn, (60, 60))
+        self.bouton_musiqueOn = Bouton(largeur - 120,0,self.bouton_musiqueOn)
+
+
+        self.bouton_changementMusique = pygame.image.load("images/changementMusique.png")
+        self.bouton_changementMusique = pygame.transform.scale(self.bouton_changementMusique, (60, 60))
+        self.bouton_changementMusique = Bouton(largeur - 180,0,self.bouton_changementMusique)
+
+
         self.hauteur = hauteur
         self.largeur = largeur
         self.plateau = Plateau()
 
     def loop(self):
+        self.test_musique = True
         self.alive = True
         
 
@@ -43,6 +60,12 @@ class Interface:            # À compléter plus tard
                     self.alive = False
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.bouton_quitter.rect.collidepoint((self.souris.rect.x, self.souris.rect.y)):
                        self.alive = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.bouton_stopmusique.rect.collidepoint((self.souris.rect.x, self.souris.rect.y)):
+                    self.test_musique = True
+
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.bouton_musiqueOn.rect.collidepoint((self.souris.rect.x, self.souris.rect.y)):
+                    self.test_musique = False
                 
                 #if event.type == pygame.KEYDOWN and event.key == KEYESCAPE:
                 #    if menu:
@@ -96,7 +119,20 @@ class Interface:            # À compléter plus tard
 
             
             self.updatePlateau()                                            # update le plateau (affiche les cartes, gère la carte en main, ect...)
+
+
+            self.bouton_quitter.update(self.frame)
+
+            if self.test_musique == True:
+
+                self.bouton_musiqueOn.update(self.frame)
+            if self.test_musique == False:
+                self.bouton_stopmusique.update(self.frame)
+
             self.souris.update(self.frame)                                  # Affiche la Souris depuis souris.py
+
+
+
             pygame.display.flip()                                           # refresh l'image à partir du nouvel état de self.groupe
 
 
@@ -109,4 +145,6 @@ class Interface:            # À compléter plus tard
             self.plateau.carte_en_main[0].deplacer(self.vitesseCurseurX, self.vitesseCurseurY)
             self.plateau.carte_en_main.draw(self.frame)
 
-        self.bouton_quitter.update(self.frame)
+
+
+        
